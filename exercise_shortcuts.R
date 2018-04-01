4f3b04ee190eac79fbb09b6f32b058de86a13b10
papelr <robertpapel@gmail.comgit config --global user.name papelrgit config --global user.email robertpapel@gmail.com>
2018-04-01 16:59:35 -0400

    kjsadfhkajs

--- exercise_shortcuts.R
+++ exercise_shortcuts.R
@@ -1,7 +1,15 @@
 # R Shortcuts with Team Lateshows
 # April 3, 2018
 # DJ Jazzy Jeff
-# 
-library(tidyverse)
- yifan
+
+
+# RStudio Shortcuts ------------------------------------------------
+
+# Description       Windows & Linux       Mac 
+# ----------------------------------------------------------------
+# Run current line  Ctrl+Enter            Command+Enter
+# Previous plot 	  Ctrl+Alt+F11 	        Command+Option+F11
+#                   Ctrl+Shift+PageUp
+# Next plot 	      Ctrl+Alt+F12 	        Command+Option+F12
+#                   Ctrl+Shift+PageDown
 
