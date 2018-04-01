8c778e4f8100a639112b2fec5e72afcac6ab9c17
papelr <robertpapel@gmail.comgit config --global user.name papelrgit config --global user.email robertpapel@gmail.com>
2018-04-01 17:15:15 -0400

    first basic outline

--- exercise_shortcuts.R
+++ exercise_shortcuts.R
@@ -1,15 +1,36 @@
 # R Shortcuts with Team Lateshows
 # April 3, 2018
 # Kristina Kelhofer, Noah Rodammer, Yifan Wang, Robert Papel, Ousmane Diaby
+# 1980s? Maybe not so much!
 
 
 # RStudio Shortcuts ------------------------------------------------
 
-# Description       Windows & Linux       Mac 
+# Description       Windows               Mac 
 # ----------------------------------------------------------------
 # Run current line  Ctrl+Enter            Command+Enter
 # Previous plot 	  Ctrl+Alt+F11 	        Command+Option+F11
 #                   Ctrl+Shift+PageUp
 # Next plot 	      Ctrl+Alt+F12 	        Command+Option+F12
 #                   Ctrl+Shift+PageDown
+#
+#
+# etc........... put our other shortcuts here? or in this format?
+
+
+# Libraries -------------------------------------------------------
+
+library(completejourney)
+library(ggplot2)
+library(tidyverse)
+
+# Exercises -------------------------------------------------------
+
+#' **Data Wrangling**
+
+#' **Data Visualization**
+
+#' **Data Modeling** 
+
+
 
